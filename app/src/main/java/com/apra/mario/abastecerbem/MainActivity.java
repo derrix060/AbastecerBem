47265d08bd1e842e89b866aa78de70403feb8a6e
derrix060 <mario_surfing@hotmail.com>
2016-03-22 23:55:32 -0300

    Added files via upload

---
+++ app/src/main/java/com/apra/mario/abastecerbem/MainActivity.java
@@ -0,0 +1,93 @@
+package com.apra.mario.abastecerbem;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Qualquer dúvida entre em contato com mariotapra@gmail.com", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final EditText gasolina, alcool;
+        final TextView resultado = (TextView) findViewById(R.id.txtResultado);
+
+        gasolina = (EditText) findViewById(R.id.txtGasolina);
+        alcool = (EditText) findViewById(R.id.txtAlcool);
+        Button btCalcular = (Button) findViewById(R.id.btCalcular);
+        Button btLimpar = (Button) findViewById(R.id.btLimpar);
+
+        btCalcular.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                double vlrAlcool, vlrGasolina;
+
+                vlrGasolina = 0.7 * Double.parseDouble(gasolina.getText().toString());
+                vlrAlcool = Double.parseDouble(alcool.getText().toString());
+
+                if (vlrGasolina > vlrAlcool){
+                    // Alcool é mais vantajoso
+                    resultado.setText("Abasteça com Alcool");
+                }
+                else{
+                    resultado.setText("Abasteça com Gasolina");
+                }
+            }
+
+        });
+
+        btLimpar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                resultado.setText("");
+                gasolina.setText("");
+                alcool.setText("");
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
